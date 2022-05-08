b9e4634df9f295d97bfa180f624e91fe693089aa
Tuvix Shih <tuvix@ninthday.info>
2022-05-08 23:37:51 +0800

    refactor: 修正寫法及加入印出嘗試次數

--- get_sensordata.py
+++ get_sensordata.py
@@ -6,37 +6,40 @@ from lywsd03mmc import Lywsd03mmcClient
 from pathlib import Path
 from time import sleep
 
-dir_path = Path(__file__).resolve().parent
-config = configparser.ConfigParser()
-config.read("{}/config.ini".format(dir_path))
-
-devices_filepath = "{}/devices.json".format(dir_path)
-try:
-    with open(devices_filepath, "r") as file:
-        devices = json.load(file)
-except FileNotFoundError as err:
-    print("Exception:" + repr(err))
-    devices = None
-
 
 def get_data(mac_address: str):
     client = Lywsd03mmcClient(mac_address)
+    try_times = 0
     loop = True
     while loop:
         try:
             data = client.data
-            print("Temperature: " + str(data.temperature))
-            print("Humidity: " + str(data.humidity))
-            print("Battery: " + str(data.battery))
-            print("Display units: " + client.units)
             loop = False
+            del client
         except BTLEDisconnectError as err:
             print("Error:" + repr(err))
+            try_times += 1
+            print("--> Try time: {}".format(try_times))
             sleep(1)
             continue
     return data
 
 
 if __name__ == "__main__":
+    dir_path = Path(__file__).resolve().parent
+    config = configparser.ConfigParser()
+    config.read("{}/config.ini".format(dir_path))
+
+    devices_filepath = "{}/devices.json".format(dir_path)
+    try:
+        with open(devices_filepath, "r") as file:
+            devices = json.load(file)
+    except FileNotFoundError as err:
+        print("Exception:" + repr(err))
+        devices = None
+
     for device in devices["lywsd03mmc"]:
         data = get_data(device["mac"])
+        print("Temperature: " + str(data.temperature))
+        print("Humidity: " + str(data.humidity))
+        print("Battery: " + str(data.battery))
