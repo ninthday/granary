52fc3af2b80bc80a4339862fce98e310fe733b0f
Tuvix Shih <tuvix@ninthday.info>
2022-05-08 21:05:08 +0800

    refactor: 迴圈取得資料

--- get_sensordata.py
+++ get_sensordata.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import configparser
 import json
-from pathlib import Path
+from bluepy.btle import BTLEDisconnectError
 from lywsd03mmc import Lywsd03mmcClient
+from pathlib import Path
+from time import sleep
 
 dir_path = Path(__file__).resolve().parent
 config = configparser.ConfigParser()
@@ -11,20 +13,30 @@ config.read("{}/config.ini".format(dir_path))
 devices_filepath = "{}/devices.json".format(dir_path)
 try:
     with open(devices_filepath, "r") as file:
-        devcies = json.load(file)
+        devices = json.load(file)
 except FileNotFoundError as err:
     print("Exception:" + repr(err))
     devices = None
 
 
-for device in devices["lywsd03mmc"]:
-    client = Lywsd03mmcClient(device["mac"])
+def get_data(mac_address: str):
+    client = Lywsd03mmcClient(mac_address)
+    loop = True
+    while loop:
+        try:
+            data = client.data
+            print("Temperature: " + str(data.temperature))
+            print("Humidity: " + str(data.humidity))
+            print("Battery: " + str(data.battery))
+            print("Display units: " + client.units)
+            loop = False
+        except BTLEDisconnectError as err:
+            print("Error:" + repr(err))
+            sleep(1)
+            continue
+    return data
 
-    data = client.data
-    print("Temperature: " + str(data.temperature))
-    print("Humidity: " + str(data.humidity))
-    print("Battery: " + str(data.battery))
-    print("Display units: " + client.units)
 
-
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    for device in devices["lywsd03mmc"]:
+        data = get_data(device["mac"])
